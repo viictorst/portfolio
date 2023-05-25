71c89489ebad42ea0eaf4fe1ec014aa60b59218e
viictorst <134561228+viictorst@users.noreply.github.com>
2023-05-25 07:36:24 -0300

    Update typing.js

--- js/modules/typing.js
+++ js/modules/typing.js
@@ -1,6 +1,6 @@
 export default function initTyping() {
     let letter = 0;
-    const text = '     Gabriela Souza';
+    const text = '     Victor Silva;
 
     function typeWriter() {
         if(letter < text.length) {
