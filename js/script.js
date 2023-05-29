1e9d2a48b939896bcd312dddc2ab59512cd66f1d
viictorst <134561228+viictorst@users.noreply.github.com>
2023-05-29 00:19:59 -0300

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,15 +1,24 @@
-import initAnimationScroll from "./modules/animacao-scroll.js";
-import initMenuMobile from "./modules/menu-mobile.js";
-import initMenuTransition from "./modules/menu-transition.js";
-import initScrollSuave from "./modules/scroll-suave.js";
-import initTabNav from "./modules/tabnav.js";
-import initTyping from "./modules/typing.js";
+// Scroll sections active
 
+let sections = document.querySelectorAll('section');
+let navLinks = document.querySelectorAll('header nav a');
 
+window.onscroll = () => {
+    sections.forEach(sec => {
+        let top = window.scrollY;
+        let offset = sec.offsetTop - 150;
+        let height = sec.offsetHeight;
+        let id = sec.getAttribute('id');
 
-initTabNav();
-initScrollSuave();
-initAnimationScroll();
-initTyping();
-initMenuTransition();
-initMenuMobile();
+        if(top >= offset && top < offset + height) {
+            navLinks.forEach(links => {
+                links.classList.remove('active');
+                document.querySelector('header nav a[href*=' + id + ']').classList.add('active');
+            });
+        };
+    });
+    // Sticky navbar
+    let header = document.querySelector('header');
+
+    header.classList.toggle('sticky', window.scrollY > 100);
+};
