7e049a725d58ba6e313c8de26d1a6ccd6e3aac10
viictorst <134561228+viictorst@users.noreply.github.com>
2023-05-29 16:19:01 -0300

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,3 +1,12 @@
+// Toggle icon navbar
+let menuIcon = document.querySelector('#menu-icon');
+let navbar = document.querySelector('.navbar');
+
+menuIcon.onclick = () => {
+    menuIcon.classList.toggle('bx-x');
+    navbar.classList.toggle('active');
+}
+
 // Scroll sections active
 
 let sections = document.querySelectorAll('section');
@@ -21,4 +30,30 @@ window.onscroll = () => {
     let header = document.querySelector('header');
 
     header.classList.toggle('sticky', window.scrollY > 100);
+
+    // Remove toggle icon and navbar when click navbar link (scroll)
+    menuIcon.classList.remove('bx-x');
+    navbar.classList.remove('active');
 };
+
+// Scroll reveal
+ScrollReveal ({
+    // reset: true,
+    distance: '80px',
+    duration: 2000,
+    delay: 200
+});
+
+ScrollReveal().reveal('.home-content, .heading', { origin: 'top' });
+ScrollReveal().reveal('.home-img, .services-container, .portfolio-box, .contact form', { origin: 'bottom' });
+ScrollReveal().reveal('.home-content h1, .about-img', { origin: 'left' });
+ScrollReveal().reveal('.home-content p, .about-content', { origin: 'right' });
+
+// Typed JS
+const typed = new Typed('.multiple-text', {
+    strings: ['Desenvolvedor Front-End', 'Designer Gráfico'],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 1000,
+    loop: true
+});
