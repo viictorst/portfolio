c2563c9829bd64b0d588d103265e71a6eec0652b
viictorst <134561228+viictorst@users.noreply.github.com>
2023-06-10 22:19:25 -0300

    Update portfólio

--- js/script.js
+++ js/script.js
@@ -1,10 +1,14 @@
+// menu icon navbar 
+
+
+
 // Toggle icon navbar
 let menuIcon = document.querySelector('#menu-icon');
 let navbar = document.querySelector('.navbar');
 
 menuIcon.onclick = () => {
-    menuIcon.classList.toggle('bx-x');
-    navbar.classList.toggle('active');
+  menuIcon.classList.toggle('bx-x');
+  navbar.classList.toggle('active');
 }
 
 // Scroll sections active
@@ -13,35 +17,35 @@ let sections = document.querySelectorAll('section');
 let navLinks = document.querySelectorAll('header nav a');
 
 window.onscroll = () => {
-    sections.forEach(sec => {
-        let top = window.scrollY;
-        let offset = sec.offsetTop - 150;
-        let height = sec.offsetHeight;
-        let id = sec.getAttribute('id');
+  sections.forEach(sec => {
+    let top = window.scrollY;
+    let offset = sec.offsetTop - 150;
+    let height = sec.offsetHeight;
+    let id = sec.getAttribute('id');
 
-        if(top >= offset && top < offset + height) {
-            navLinks.forEach(links => {
-                links.classList.remove('active');
-                document.querySelector('header nav a[href*=' + id + ']').classList.add('active');
-            });
-        };
-    });
-    // Sticky navbar
-    let header = document.querySelector('header');
+    if (top >= offset && top < offset + height) {
+      navLinks.forEach(links => {
+        links.classList.remove('active');
+        document.querySelector('header nav a[href*=' + id + ']').classList.add('active');
+      });
+    };
+  });
+  // Sticky navbar
+  let header = document.querySelector('header');
 
-    header.classList.toggle('sticky', window.scrollY > 100);
+  header.classList.toggle('sticky', window.scrollY > 50);
 
-    // Remove toggle icon and navbar when click navbar link (scroll)
-    menuIcon.classList.remove('bx-x');
-    navbar.classList.remove('active');
+  // Remove toggle icon and navbar when click navbar link (scroll)
+  menuIcon.classList.remove('bx-x');
+  navbar.classList.remove('active');
 };
 
 // Scroll reveal
-ScrollReveal ({
-    // reset: true,
-    distance: '80px',
-    duration: 2000,
-    delay: 200
+ScrollReveal({
+  // reset: true,
+  distance: '80px',
+  duration: 2000,
+  delay: 200
 });
 
 ScrollReveal().reveal('.home-content, .heading', { origin: 'top' });
@@ -51,18 +55,20 @@ ScrollReveal().reveal('.home-content p, .about-content', { origin: 'right' });
 
 // Typed JS
 const typed = new Typed('.multiple-text', {
-    strings: ['Desenvolvedor Front-End', 'Designer Gráfico'],
-    typeSpeed: 100,
-    backSpeed: 100,
-    backDelay: 1000,
-    loop: true
+  strings: ['Desenvolvedor Front-End', 'Designer Gráfico'],
+  typeSpeed: 100,
+  backSpeed: 100,
+  backDelay: 1000,
+  loop: true
 });
 
-// Animação imagem home
+
+
+
 var container = document.querySelector('.home-img');
 var img = document.getElementById('img');
 
-container.addEventListener('mousemove', function(e) {
+container.addEventListener('mousemove', function (e) {
   var rect = container.getBoundingClientRect();
   var x = e.clientX - rect.left; // Coordenada X do mouse dentro do contêiner
   var y = e.clientY - rect.top; // Coordenada Y do mouse dentro do contêiner
@@ -74,6 +80,34 @@ container.addEventListener('mousemove', function(e) {
   img.style.transform = 'perspective(1000px) rotateX(' + rotateX + 'deg) rotateY(' + rotateY + 'deg)';
 });
 
-container.addEventListener('mouseout', function() {
+container.addEventListener('mouseout', function () {
   img.style.transform = 'none'; // Redefine a transformação para o estado original
 });
+
+
+
+// swiper
+var swiper = new Swiper(".mySwiper", {
+  slidesPerView: 1,
+  spaceBetween: 50,
+  loop: true,
+  grabCursor: true,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
+
+
+
+// Dark mode
+let darkModeIcon = document.querySelector('#darkMode-icon');
+
+darkModeIcon.onclick = () => {
+  darkModeIcon.classList.toggle('bx-sun');
+  document.body.classList.toggle('dark-mode')
+}
